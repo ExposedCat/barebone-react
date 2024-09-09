74c6c3c98115c1b16b0a5ccc2e9d0fb33cb6a98b
Artem Prokop <artem13.prokop@gmail.com>
2024-09-10 01:20:21 +0200

    feat: panda css

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
 import { useState } from 'react';
 
-import viteLogo from '../../../../../../vite.svg';
-import reactLogo from './assets/react.svg';
+import { css } from '../public/styled-system/css/css.mjs';
+import reactLogo from './public/react.svg';
+import { Flex } from './components/Flex.js';
 
-import './App.css';
-
-function App() {
+export function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <Flex direction="column" full align="center" justify="center">
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+      <h1>React Essential</h1>
+      <Flex direction="column" align="center">
+        <button
+          onClick={() => setCount(count => count + 1)}
+          className={css({
+            backgroundColor: 'blue.200',
+            padding: '1',
+            rounded: 'md',
+          })}
+        >
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </Flex>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </Flex>
   );
 }
-
-export default App;
